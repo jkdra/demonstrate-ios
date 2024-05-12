e0f9fdd2ae23f5c65fadd0da760b516c43435e3b
Jawad Khadra <jawad.alkhadra@gmail.com>
2024-05-12 11:01:07 -0700

    additional fixes to make sure it runs

---
+++ Demonstrate/UI/Loading Indicators/ShimmerEffect.swift
@@ -0,0 +1,18 @@
+//
+//  ShimmerEffect.swift
+//  Demonstrate
+//
+//  Created by Jawad Khadra on 5/12/24.
+//
+
+import SwiftUI
+
+struct ShimmerEffect: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShimmerEffect()
+}
--- DemonstrateTests/demonstrateTests.swift
+++ DemonstrateTests/demonstrateTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-@testable import demonstrate
+@testable import Demonstrate
 
 final class demonstrateTests: XCTestCase {
 
