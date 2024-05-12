acdaad26a8a961687f62ca6e388de2879ab2c105
Jawad Khadra <jawad.alkhadra@gmail.com>
2024-05-12 10:51:43 -0700

    yes

--- Demonstrate/Supabase Client.swift
+++ Demonstrate/Supabase Client.swift
@@ -13,8 +13,10 @@ let devSupabase = SupabaseClient(
     supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
 )
 
-let supabase = SupabaseClient(supabaseURL: URL(string: "https://tcratutdtunzzyyzylwe.supabase.co")!,
-                              supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjcmF0dXRkdHVuenp5eXp5bHdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUzMjMwMTgsImV4cCI6MjAzMDg5OTAxOH0.JV5tnFSy4xm7wxh-LaulKqUFwWkTyqGHaYOfNOZDz8c")
+let supabase = SupabaseClient(
+    supabaseURL: URL(string: "https://tcratutdtunzzyyzylwe.supabase.co")!,
+    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjcmF0dXRkdHVuenp5eXp5bHdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUzMjMwMTgsImV4cCI6MjAzMDg5OTAxOH0.JV5tnFSy4xm7wxh-LaulKqUFwWkTyqGHaYOfNOZDz8c"
+)
 
 let database = devSupabase.schema("public")
 let auth = devSupabase.auth
