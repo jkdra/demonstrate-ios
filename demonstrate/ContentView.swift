b3cbea0896141aab80b00af0215232386029a2df
jawadkhadra <jawad.alkhadra@gmail.com>
2025-01-13 16:15:05 +0300

    Initial Commit

---
+++ demonstrate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demonstrate
+//
+//  Created by Jawad Khadra on 1/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demonstrate/demonstrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  demonstrateApp.swift
+//  demonstrate
+//
+//  Created by Jawad Khadra on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct demonstrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
